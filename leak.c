a896e141c981d5938f7a9b41dadb7d5a79851eeb
Jules-Arthus KLEIN <jules.arthus.klein@gmail.com>
2025-08-25 16:05:59 +0200

    JAI FIX LE DOCKER HAHAHAHAHAHAHAHAHAHAHHAHAHAHAH

--- leak.c
+++ leak.c
@@ -1,6 +1,6 @@
 #include <stdlib.h>
 
 int main(int argc, char *argv[]) {
-  int *a = malloc(5);
+  int* a = malloc(50000);
   return EXIT_SUCCESS;
 }
--- noLeak.c
+++ noLeak.c
@@ -1,3 +1,1 @@
-#include <stdlib.h>
-
-int main(int argc, char *argv[]) { return EXIT_SUCCESS; }
+int main(int argc, char *argv[]) { return 0; }
